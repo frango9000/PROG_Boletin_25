9aef5bed1beb4dc5e9a2ed03559181baf2135c6c
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2019-04-02 11:41:05 +0200

    fin

--- prog_boletin_25/Main.java
+++ prog_boletin_25/Main.java
@@ -3,9 +3,21 @@ package prog_boletin_25;
  * @author fsancheztemprano
  */
 public class Main {
+    static NuevoUsuario v;
+    static Devoluciones d;
     public static void main(String[] args) {
-        
-
-
+        v = new NuevoUsuario();  
+        d = new Devoluciones();
+        v.setVisible(true);      
+    }
+    
+    public static void control(int i){
+        if(i == 0){
+            v.setVisible(false);
+            d.setVisible(true);
+        }else{
+            v.setVisible(true);
+            d.setVisible(false);
+        }
     }
 }
